d8a098f4b03e560bd125ffb3a062f62d9c283ff0
Mohasin K R <30471994+mohasinkr@users.noreply.github.com>
2022-08-14 18:56:56 +0530

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+
+const Header = ({title})=>{
+return (
+    <div>
+        <h1 className='simple'>{title}</h1>
+    </div>
+);
+}
+
+Header.defaultProps = {
+    title:"Task Tracker",
+};
+
+Header.propTypes = {
+    title : PropTypes.string.isRequired,
+};
+
+export default Header;
