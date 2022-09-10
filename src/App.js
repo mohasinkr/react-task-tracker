ab589bc114597db8dd7733b357a33a051841cc15
Mohasin K R <30471994+mohasinkr@users.noreply.github.com>
2022-09-10 14:56:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
   return (
-  <Header/>
+    <div className='container'>
+     <Header/>
+    <Tasks/>
+    </div>
   );
 }
 
